1f06b7a8d71c9870254b7359df69f8c0b832e23b
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-14 18:00:01 -0300

    Estilizando componente comentários

--- src/components/Comentarios.vue
+++ src/components/Comentarios.vue
@@ -8,8 +8,7 @@
                     </div><!--cima-->
                     <div class="baixo">
                         
-                        <div class="imagem">
-                            <img src="../assets/profile-1.jpg" alt="">
+                        <div class="imagem imagem1">
                         </div>
                         <div class="titulos">
                             <span>Satish Patel</span>
@@ -24,8 +23,7 @@
                     </div><!--cima-->
                     <div class="baixo">
                         
-                        <div class="imagem">
-                            <img src="../assets/profile-2.jpg" alt="">
+                        <div class="imagem imagem2">       
                         </div>
                         <div class="titulos">
                             <span>Bruce McKenzie</span>
@@ -40,8 +38,7 @@
                     </div><!--cima-->
                     <div class="baixo">
                         
-                        <div class="imagem">
-                            <img src="../assets/profile-3.jpg" alt="">
+                        <div class="imagem imagem3">
                         </div>
                         <div class="titulos">
                             <span>Iva Boyd</span>
@@ -63,3 +60,62 @@ export default defineComponent({
     name: 'Comentarios-landing-page',
 })
 </script>
+
+<style scoped>
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #181F2B;
+    }
+    .box{
+        width: 70%;
+        background-image: url(../assets/bg-quotes.png);
+        background-repeat: no-repeat ;
+        display: flex;
+    }
+    .comentarios{
+        margin: 40px 20px 40px 10px;
+        background: #21293C;
+        box-shadow: 10px 10px 5px 0px #141724;
+        border-radius: 5px;
+    }
+    .cima p{
+        padding: 40px 30px 25px 30px;
+        color: white;
+        font-family: 'Open Sans';
+        font-size: 17px;
+        line-height: 20px;
+    }
+    .baixo{
+        display: flex;
+        padding-bottom: 30px ;
+    }
+    .titulos{
+        display:flex;
+        flex-direction: column;
+        align-items: right;
+        margin-left: 10px;
+        font-family: 'Open Sans';
+        color: white;
+    }
+    .titulos span:first-child{
+        font-size: 14px;
+        font-weight: bold;
+    }
+    .titulos span:last-child{
+        font-size: 10px;
+        margin-top: 3px;
+    }
+    .imagem{
+        width: 30px;
+        height: 30px;
+        border-radius: 15px;
+        background-size: cover;
+        margin-left: 30px;
+    }
+    .imagem1{background-image: url(../assets/profile-1.jpg);}
+    .imagem2{background-image: url(../assets/profile-2.jpg);}
+    .imagem3{background-image: url(../assets/profile-3.jpg);}
+
+</style>
