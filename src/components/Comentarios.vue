14fca1d0023239d6f0f758bcb36c28e698cb28e9
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-14 17:26:34 -0300

    Estruturando componente comentarios

--- src/components/Comentarios.vue
+++ src/components/Comentarios.vue
@@ -1,5 +1,58 @@
 <template>
-    <h1>ola</h1>
+    <div class="container">
+        
+        <div class="box">
+                <div class="comentarios">
+                    <div class="cima">
+                        <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-olled collaboration machine.</p>
+                    </div><!--cima-->
+                    <div class="baixo">
+                        
+                        <div class="imagem">
+                            <img src="../assets/profile-1.jpg" alt="">
+                        </div>
+                        <div class="titulos">
+                            <span>Satish Patel</span>
+                            <span>Founder & CEO, Huddle</span>
+                        </div><!--titulos-->
+                    
+                    </div><!--baixo-->
+                </div><!--comentarios-->
+                <div class="comentarios">
+                    <div class="cima">
+                        <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-olled collaboration machine.</p>
+                    </div><!--cima-->
+                    <div class="baixo">
+                        
+                        <div class="imagem">
+                            <img src="../assets/profile-2.jpg" alt="">
+                        </div>
+                        <div class="titulos">
+                            <span>Bruce McKenzie</span>
+                            <span>Founder & CEO, Huddle</span>
+                        </div><!--titulos-->
+                    
+                    </div><!--baixo-->
+                </div><!--comentarios-->
+                <div class="comentarios">
+                    <div class="cima">
+                        <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-olled collaboration machine.</p>
+                    </div><!--cima-->
+                    <div class="baixo">
+                        
+                        <div class="imagem">
+                            <img src="../assets/profile-3.jpg" alt="">
+                        </div>
+                        <div class="titulos">
+                            <span>Iva Boyd</span>
+                            <span>Founder & CEO, Huddle</span>
+                        </div><!--titulos-->
+                    
+                    </div><!--baixo-->
+                </div><!--comentarios-->
+        </div><!--box-->
+
+    </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +60,6 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'Comentários',
+    name: 'Comentarios-landing-page',
 })
 </script>
